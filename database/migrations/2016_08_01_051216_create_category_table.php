eaca55480d437c622c2b815ad4afec687dcc8eb9
Mobarok Hossen <mobarok_hossen@outlook.com>
2016-09-29 18:14:39 +0600

    Final Newspaper
    Signed-off-by: Mobarok Hossen <mobarok_hossen@outlook.com>

---
+++ database/migrations/2016_08_01_051216_create_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name');
+            $table->text('category_description');
+            $table->tinyInteger('deletion_status');
+            $table->tinyInteger('publication_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tbl_category');
+    }
+}
